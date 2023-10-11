7b84d2b7a73bb125eaea81a4de61b9bb974a9fba
Crystal Luong <redtriger213221@gmail.com>
2023-10-11 16:12:33 -0400

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello
 {
 public static void main( String argv[] )
  {
- System.out.println( "“hello CSCI 4485, " + argv[0] + "!" );
+ System.out.println( "hello CSCI 4485" + argv[0] + "!" );
 }
 }
